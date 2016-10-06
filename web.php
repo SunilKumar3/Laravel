ca5741ba2358635a29a6695a5528e440c7e57586
SunilKumar3 <ds.sunil9@yahoo.co.in>
2016-10-06 08:49:07 -0700

    Add files via upload

---
+++ web.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+
+Route::get('/','PagesController@index');
+Route::get('/about','PagesController@about');
+
+Route::get('blade','PagesController@blade'); 
+
+Route::get('profile','PagesController@profile');
+Route::get('settings','PagesController@settings');
+
+Route::get('/','WelcomeController@index');
+
+Route::get('/home','HomeController@index');
+
+Route::get('users', ['uses' =>'UsersController@index']);
+Route::get('users/create',['uses' => 'userscontroller@create']);
+Route::post('users',['uses' => 'userscontroller@store']);
+
+/*
+Route::get('users', function(){
+	$users =[
+'0' =>[
+	'first_name' => 'Renato',
+	'last_name' => 'Hysa',
+	'location' => 'Albania'
+],
+
+'1' => [
+	'first_name' => 'Jessica',
+	'last_name' => 'Alba',
+	'location' => 'USA'
+] 
+
+	];
+
+	return $users;
+});
+*/
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
